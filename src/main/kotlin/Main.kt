677467f13eda2c7c55bff14333fa1380f7b686ac
Rajashreemallik2 <rajashreemallik2@gmail.com>
2025-08-06 23:07:03 +0530

    final commit of Project Completion.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,6 @@ import org.example.controller.LoanController
 import org.example.services.BankService
 import org.example.view.ConsoleView
 
-
     fun main() {
         val controller = LoanController(BankService(), ConsoleView())
         controller.startLoanProcess()
